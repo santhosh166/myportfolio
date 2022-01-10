ee188bd1b24d4177b4e1f533f0f236d8a1706b60
Santhosh R S <65718917+santhosh166@users.noreply.github.com>
2022-01-10 19:38:39 +0530

    Update settings.py

--- myprofile/settings.py
+++ myprofile/settings.py
@@ -142,7 +142,7 @@ STATIC_ROOT=os.path.join(BASE_DIR,'assests')
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), '..', 'media').replace('\\','/')
 
 
 # Default primary key field type
