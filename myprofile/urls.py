658065df086105c3e03a7765dae26c62cebaf82b
Santhosh R S <65718917+santhosh166@users.noreply.github.com>
2022-01-10 19:51:14 +0530

    Update urls.py

--- myprofile/urls.py
+++ myprofile/urls.py
@@ -22,5 +22,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('cv.urls')),
-]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+]
+#if settings.DEBUG:
+ # urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+  #urlpatterns+=static(settings.STATIC_URL, document_root = settings.STATICFILES_DIRS[0])
+  
 
