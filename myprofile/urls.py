0f6bb7b189caebf2966e00807d8f8482c6ee4710
Santhosh R S <65718917+santhosh166@users.noreply.github.com>
2022-01-10 20:01:50 +0530

    Update urls.py

--- myprofile/urls.py
+++ myprofile/urls.py
@@ -23,8 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('cv.urls')),
 ]
-#if settings.DEBUG:
- # urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-  #urlpatterns+=static(settings.STATIC_URL, document_root = settings.STATICFILES_DIRS[0])
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
   
 
