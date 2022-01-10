8c47341bd8752b318c6e7aba1d475f8f21f302a5
Santhosh R S <65718917+santhosh166@users.noreply.github.com>
2022-01-10 20:09:48 +0530

    Update urls.py

--- myprofile/urls.py
+++ myprofile/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('cv.urls')),
 ]
+urlpatterns += [ url(r'^uploads/(?P.)$', serve,{'document_root': settings.MEDIA_ROOT}), url(r'^static/(?P.)$', serve,{'document_root': settings.STATIC_ROOT}), ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
   
