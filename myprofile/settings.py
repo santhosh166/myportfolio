5aa08408548bfdb3b64c8784db752a7427f92b32
Santhosh R S <65718917+santhosh166@users.noreply.github.com>
2022-01-11 10:28:34 +0530

    Update settings.py

--- myprofile/settings.py
+++ myprofile/settings.py
@@ -48,7 +48,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cv'
+    'cv',
+    'cloudinary',
+
+    'cloudinary_storage',
 ]
 
 MIDDLEWARE = [
